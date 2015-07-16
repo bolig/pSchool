b72012948861b738ad66147f331d96f7e20df4ba
liVincent <1043973279@qq.com>
2015-07-16 15:28:02 +0800

    libo

--- app/src/main/java/com/peoit/android/online/pschool/ui/activity/BasicInfoActivity.java
+++ app/src/main/java/com/peoit/android/online/pschool/ui/activity/BasicInfoActivity.java
@@ -1,5 +1,8 @@
 package com.peoit.android.online.pschool.ui.activity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.peoit.android.online.pschool.R;
@@ -8,16 +11,19 @@ import com.peoit.android.online.pschool.ui.Base.BaseActivity;
 /**
  * Created by ZYZ on 2015/7/16.
  */
-public class BasicInformation extends BaseActivity{
+public class BasicInfoActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basicinformation);
         getPsActionBar().settitle("基本资料");
-
     }
 
+    public static void startThisActivity(Activity mAc){
+        Intent intent = new Intent(mAc, BasicInfoActivity.class);
+        mAc.startActivity(intent);
+    }
 
     @Override
     public void initData() {
@@ -31,11 +37,6 @@ public class BasicInformation extends BaseActivity{
 
     @Override
     public void initListener() {
-
-    }
-
-    @Override
-    public void responseSuccess(Object responses) {
 
     }
 }
