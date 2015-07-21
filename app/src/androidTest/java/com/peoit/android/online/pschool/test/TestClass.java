986958f6f8e5a6fa58c2f43bebeccd165e24bbfe
linghp <421620330@qq.com>
2015-07-21 15:49:30 +0800

    Merge branch 'master' of https://github.com/l612714/pSchool
    Conflicts:
    	app/app.iml
    	app/src/main/java/com/peoit/android/online/pschool/net/base/GsonRequest.java

--- app/src/androidTest/java/com/peoit/android/online/pschool/test/TestClass.java
+++ app/src/androidTest/java/com/peoit/android/online/pschool/test/TestClass.java
@@ -1,10 +1,17 @@
 package com.peoit.android.online.pschool.test;
 
+import android.test.InstrumentationTestCase;
+
 /**
  * author:libo
  * time:2015/7/15
  * E-mail:boli_android@163.com
  * last: ...
  */
-public class TestClass {
+public class TestClass extends InstrumentationTestCase {
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 2;
+        assertEquals(expected, reality);
+    }
 }
