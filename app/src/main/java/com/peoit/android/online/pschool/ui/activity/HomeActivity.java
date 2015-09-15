2f08d6a8e739a96c79bb3e3e2769a24efc18daee
zyz <1125213018@qq.com>
2015-09-15 09:46:06 +0800

    no change

--- app/src/main/java/com/peoit/android/online/pschool/ui/activity/HomeActivity.java
+++ app/src/main/java/com/peoit/android/online/pschool/ui/activity/HomeActivity.java
@@ -1,15 +1,15 @@
 package com.peoit.android.online.pschool.ui.activity;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -17,42 +17,27 @@ import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
-import com.easemob.EMCallBack;
 import com.easemob.EMEventListener;
 import com.easemob.EMNotifierEvent;
 import com.easemob.applib.controller.HXSDKHelper;
 import com.easemob.chat.EMChatManager;
 import com.easemob.chat.EMConversation;
-import com.easemob.chat.EMGroup;
-import com.easemob.chat.EMGroupManager;
 import com.easemob.chat.EMMessage;
-import com.easemob.chatuidemo.Constant;
-import com.easemob.chatuidemo.DemoApplication;
-import com.easemob.chatuidemo.db.UserDao;
-import com.easemob.chatuidemo.domain.User;
 import com.easemob.chatuidemo.utils.CommonUtils;
-import com.easemob.exceptions.EaseMobException;
 import com.peoit.android.online.pschool.R;
 import com.peoit.android.online.pschool.config.CommonUtil;
-import com.peoit.android.online.pschool.config.Constants;
-import com.peoit.android.online.pschool.entity.UserInfo;
 import com.peoit.android.online.pschool.ui.Base.BaseActivity;
-import com.peoit.android.online.pschool.ui.Base.PsApplication;
+import com.peoit.android.online.pschool.ui.Presenter.HXHelperPresenter;
 import com.peoit.android.online.pschool.ui.Presenter.HomeBannerPresenter;
 import com.peoit.android.online.pschool.ui.Presenter.HomeItemPresenter;
 import com.peoit.android.online.pschool.ui.Presenter.HomePersenter;
-import com.peoit.android.online.pschool.ui.adapter.ImageSliderAdapter;
 import com.peoit.android.online.pschool.ui.view.PsActionBar;
 import com.peoit.android.online.pschool.utils.MyLogger;
+import com.peoit.android.online.pschool.utils.NetWorkHelper;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Hashtable;
-import java.util.List;
-import java.util.Map;
 import java.util.Timer;
 
 /**
@@ -64,30 +49,15 @@ import java.util.Timer;
  * last: ...
  */
 public class HomeActivity extends BaseActivity implements ViewPagerEx.OnPageChangeListener, View.OnClickListener, EMEventListener {
-    private int currentItem = Integer.MAX_VALUE / 2;
-    private int[] imgs;
-    private List<View> views = new ArrayList<>();
     public static HomeActivity instance;
     private PsActionBar actionbar;
-    private DrawerLayout mDrawerLayout;
-//    private SliderLayout mViewPager;
-//    private PagerIndicator mLl_point;
-    private ImageSliderAdapter mSliderAdapter;
+    public DrawerLayout mDrawerLayout;
     private View layout_imageSlider;
     private FrameLayout mLayout_body;
-//    private Map<String, Integer> res_files;
-////    private LinearLayout ll_item1;
-////    private LinearLayout ll_item2;
-// //   private LinearLayout ll_item3;
-///    private LinearLayout ll_item4;
-//    private LinearLayout ll_item5;
-//    private LinearLayout ll_item6;
 
     private ListView dataList;
 
-//    private TextView tv_unread_msg_number;
     private int childWitd;
-//   private static String chatname, groupid;
     private Timer timer_sys_check;
     private Handler handler = new Handler() {
         public void handleMessage(Message msg) {
@@ -99,33 +69,45 @@ public class HomeActivity extends BaseActivity implements ViewPagerEx.OnPageChan
 
         }
     };
-    private static UserInfo userInfo;
-    private static String currentNikeName;
     private HomePersenter mPersenter;
     private TextView logout;
     private GridView gv_item;
     private HomeItemPresenter homeItemPresenter;
-
-    private String groupid;
     private HomeBannerPresenter mHomeBannerPresenter;
+
+    public HXHelperPresenter mHXHelperPresneter;
 
     private LinearLayout home_ll;
     private TextView home_tv;
     private int expert = 0;
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         isMainUI = false;
         setContentView(R.layout.act_home);
         instance = this;
-//        chatname = PsApplication.getInstance().getUserName();
-//        Log.i("chatname", chatname + "");
-
         timer_sys_check = new Timer();
-        timer_sys_check.schedule(new Page_check_task(), 10000, 10000);
+        timer_sys_check.schedule(new Page_check_task(), 1000, 1000);
 
+        initReceiver();
+
+    }
+
+    private static final String action_network = "android.net.conn.CONNECTIVITY_CHANGE";
+
+    private BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (NetWorkHelper.checkNetState(mContext)){
+                mHomeBannerPresenter.doLoadBannerImg();
+                mHXHelperPresneter.login(false);
+            }
+        }
+    };
+
+    private void initReceiver() {
+        IntentFilter filter = new IntentFilter(action_network);
+        registerReceiver(myReceiver, filter);
     }
 
     class Page_check_task extends java.util.TimerTask {
@@ -149,21 +131,8 @@ public class HomeActivity extends BaseActivity implements ViewPagerEx.OnPageChan
         MyLogger.i("onResume", "onResume");
         MyLogger.i("onResume3", isLogin() + "");
         if (isLogin()) {
-            userInfo = CommonUtil.getCurrentUser();
-            if (userInfo != null) {
-                Log.i("onResume2", userInfo.toString());
-                currentUsername = userInfo.getUsername();
-                int type = CommonUtil.getIdEntityType();
-                if (type == Constants.TYPE_PARENT) {
-                    currentNikeName = userInfo.getStuname();
-                } else {
-                    currentNikeName = userInfo.getNickname();
-                }
 
-                login();
-            }
         }
-
     }
 
     @Override
@@ -175,18 +144,16 @@ public class HomeActivity extends BaseActivity implements ViewPagerEx.OnPageChan
 
     @Override
     public void initData() {
-//        res_files = new HashMap<String, Integer>();
-//        res_files.put("item0", R.mipmap.home_01);
-//        res_files.put("item1", R.mipmap.home_01);
-//        res_files.put("item2", R.mipmap.home_01);
-//        res_files.put("item3", R.mipmap.home_01);
-//        res_files.put("item4", R.mipmap.home_01);
-
         mPersenter = new HomePersenter(this);
     }
 
     @Override
     public void initView() {
+
+        mHXHelperPresneter = new HXHelperPresenter(this);
+
+        mHXHelperPresneter.login(false);
+
         actionbar = (PsActionBar) findViewById(R.id.actionbar);
         actionbar.settitle("铜仁家长网校");
         actionbar.addLeftBtn(R.mipmap.ic_menu, new View.OnClickListener() {
@@ -197,6 +164,8 @@ public class HomeActivity extends BaseActivity implements ViewPagerEx.OnPageChan
         });
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mDrawerLayout.closeDrawers();
 
         dataList = (ListView) findViewById(R.id.data_list);
 
@@ -213,23 +182,7 @@ public class HomeActivity extends BaseActivity implements ViewPagerEx.OnPageChan
 
         mLayout_body.addView(layout_imageSlider);
 
-//        ll_item1 = (LinearLayout) findViewById(R.id.homl_ll_item1);
-//        ll_item2 = (LinearLayout) findViewById(R.id.homl_ll_item2);
-//        ll_item3 = (LinearLayout) findViewById(R.id.homl_ll_item3);
-//        ll_item4 = (LinearLayout) findViewById(R.id.homl_ll_item4);
-//        ll_item5 = (LinearLayout) findViewById(R.id.homl_ll_item5);
-//        ll_item6 = (LinearLayout) findViewById(R.id.homl_ll_item6);
-//        tv_unread_msg_number = (TextView) findViewById(R.id.main_unread_msg_number);
-//        tv_unread_msg_number.setVisibility(View.GONE);
-
         childWitd = (CommonUtil.w_screeen - CommonUtil.dip2px(mContext, 2)) / 3;
-//        setLinearlayoutWidth(ll_item1);
-//        setLinearlayoutWidth(ll_item2);
-//        setLinearlayoutWidth(ll_item3);
-//        setLinearlayoutWidth(ll_item4);
-//        setLinearlayoutWidth(ll_item5);
-//        setLinearlayoutWidth(ll_item6);
-
 
         gv_item = (GridView) findViewById(R.id.gv_item);
         home_ll = (LinearLayout) findViewById(R.id.home_ll1);
@@ -242,35 +195,7 @@ public class HomeActivity extends BaseActivity implements ViewPagerEx.OnPageChan
             home_ll.setVisibility(View.GONE);
             gv_item.setVisibility(View.VISIBLE);
         }
-
-        homeItemPresenter = new HomeItemPresenter(this, gv_item, home_tv);
-
-//        int type = CommonUtil.getIdEntityType();
-//        if (type == Constants.TYPE_TEACHER){
-//            //教师端
-//        }
-//        else if (type == Constants.TYPE_PARENT){
-//        // 家长端
-//         }
-//        else if (type == Constants.TYPE_ZHUAN_JIA) {
-//            //专家端
-//            ll_item1.setVisibility(View.GONE);
-//            ll_item5.setVisibility(View.GONE);
-//        }
-//        else {
-////            throw new RuntimeException(" @libo idEntity is exception num ");
-//
-//        }
-
     }
-
-//    @Override
-//    protected void onResume() {
-//        // register the event listener when enter the foreground
-//        EMChatManager.getInstance().registerEventListener(this,
-//                new EMNotifierEvent.Event[]{EMNotifierEvent.Event.EventNewMessage, EMNotifierEvent.Event.EventOfflineMessage, EMNotifierEvent.Event.EventConversationListChanged});
-//        super.onResume();
-//    }
 
     @Override
     protected void onStop() {
@@ -287,20 +212,18 @@ public class HomeActivity extends BaseActivity implements ViewPagerEx.OnPageChan
 
     @Override
     public void initListener() {
-//        mViewPager.addOnPageChangeListener(this);
-
-//        ll_item1.setOnClickListener(this);
-//        ll_item2.setOnClickListener(this);
-//        ll_item3.setOnClickListener(this);
-//        ll_item4.setOnClickListener(this);
-//        ll_item5.setOnClickListener(this);
-//        ll_item6.setOnClickListener(this);
-
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 logout();
                 finish();
+            }
+        });
+        mHXHelperPresneter.setOnGroupIdListener(new HXHelperPresenter.OnGroupIdListener() {
+            @Override
+            public void onGroupId(String GroupId, boolean isToChat) {
+                homeItemPresenter.changeGroupId(GroupId);
+                homeItemPresenter.toChat(isToChat);
             }
         });
     }
@@ -341,204 +264,8 @@ public class HomeActivity extends BaseActivity implements ViewPagerEx.OnPageChan
 
     }
 
-//    @Override
-//    public void onSliderClick(BaseSliderView baseSliderView) {
-//        Toast.makeText(this, baseSliderView.getBundle().get("extra") + "", Toast.LENGTH_SHORT).show();
-//    }
-
     @Override
     public void onClick(View v) {
-//        if (v == ll_item1) {
-//            if (isLoginAndToLogin())
-//                //金融IC卡充值
-//                BankICActivity.startThisActivity(mContext);
-//        } else if (v == ll_item2) {
-////            //校园信息
-////            if (isLoginAndToLogin())
-////                NewsActivity.startThisActivity(mContext, NewsActivity.test, "新闻");
-//        } else if (v == ll_item3) {
-//            //校园通知
-//            if (isLoginAndToLogin())
-//                NoticeActivity.startThisActivity(mContext);
-//        } else if (v == ll_item4) {
-//            //学校专栏
-//            if (isLoginAndToLogin())
-//                FeatureActivity.startThisActivity(mContext);
-//        } else if (v == ll_item5) {
-//            //交流
-//            if (isLoginAndToLogin() && !TextUtils.isEmpty(chatname)) {
-//                Intent intent = new Intent(HomeActivity.this, ChatActivity.class);
-//                // it is group chat
-//                intent.putExtra("chatType", ChatActivity.CHATTYPE_GROUP);
-//                intent.putExtra("groupId", groupid);
-//                Log.i("chatname", chatname);
-//                Log.i("groupid", groupid);
-//                //intent.putExtra("groupId", "85759016126382492");
-//                startActivityForResult(intent, 0);
-//            }
-//        } else if (v == ll_item6) {
-//            //支付
-//
-//        }
-    }
-
-    private static String currentUsername = "xdd02";
-    private String currentPassword = "dba508b941b095bcd5060ff742a436e2";
-    private boolean progressShow;
-
-    /**
-     * 登录
-     *
-     * @param
-     */
-    public void login() {
-        if (!CommonUtils.isNetWorkConnected(this)) {
-            Toast.makeText(this, R.string.network_isnot_available, Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        if (TextUtils.isEmpty(currentUsername)) {
-            //提示用户名不能为空
-            Toast.makeText(this, R.string.User_name_cannot_be_empty, Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (TextUtils.isEmpty(currentPassword)) {
-            Toast.makeText(this, R.string.Password_cannot_be_empty, Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        progressShow = true;
-        final ProgressDialog pd = new ProgressDialog(this);
-        pd.setCanceledOnTouchOutside(false);
-        pd.setOnCancelListener(new DialogInterface.OnCancelListener() {
-
-            @Override
-            public void onCancel(DialogInterface dialog) {
-                progressShow = false;
-            }
-        });
-        pd.setMessage(getString(R.string.Is_landing));
-        pd.show();
-
-        final long start = System.currentTimeMillis();
-        // 调用sdk登陆方法登陆聊天服务器
-        EMChatManager.getInstance().login(currentUsername, currentPassword, new EMCallBack() {
-            @Override
-            public void onSuccess() {
-                if (!progressShow) {
-                    return;
-                }
-                // 登陆成功，保存用户名密码
-                PsApplication.getInstance().setUserName(currentUsername);
-                PsApplication.getInstance().setPassword(currentPassword);
-//                chatname = PsApplication.getInstance().getUserName();
-
-                try {
-                    // ** 第一次登录或者之前logout后再登录，加载所有本地群和回话
-                    // ** manually load all local groups and
-                    EMGroupManager.getInstance().loadAllGroups();
-                    EMChatManager.getInstance().loadAllConversations();
-                    // 处理好友和群组
-                    initializeContacts();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    // 取好友或者群聊失败，不让进入主页面
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            pd.dismiss();
-                            PsApplication.getInstance().logout(null);
-                            Toast.makeText(getApplicationContext(), R.string.login_failure_failed, Toast.LENGTH_LONG).show();
-                        }
-                    });
-                    return;
-                }
-                if (!HomeActivity.this.isFinishing() && pd.isShowing()) {
-                    pd.dismiss();
-                }
-                List<EMGroup> grouplist = null;
-                try {
-                    grouplist = EMGroupManager.getInstance().getGroupsFromServer();
-                } catch (EaseMobException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                if (grouplist != null && grouplist.size() > 0) {
-                    Log.i("grouplist", grouplist.size() + "---" + grouplist.toString());
-                    Log.i("currentNikeName456", currentNikeName);
-
-                    groupid = grouplist.get(0).getGroupId();
-                    List<String> list = grouplist.get(0).getMembers();
-                    String mebers = null;
-                    for (int i = 0; list != null && i < list.size(); i++) {
-                        if (i == list.size() - 1){
-                            mebers += list.get(i);
-                        } else {
-                            mebers += list.get(i) + ",";
-                        }
-                    }
-
-                    getShare().put(Constants.LOGIN_GROUP_ID, mebers);
-
-                    homeItemPresenter.changeGroupId(groupid);
-
-                    DemoApplication.getInstance().setNickName(currentNikeName);
-                } else {
-                    //showToast("你尚未被添加进任何群组, 给你");
-                }
-            }
-
-            @Override
-            public void onProgress(int progress, String status) {
-            }
-
-            @Override
-            public void onError(final int code, final String message) {
-                if (!progressShow) {
-                    return;
-                }
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        pd.dismiss();
-                        Toast.makeText(getApplicationContext(), getString(R.string.Login_failed) + message,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-    }
-
-    private void initializeContacts() {
-        Map<String, User> userlist = new HashMap<String, User>();
-        // 添加user"申请与通知"
-        User newFriends = new User();
-        newFriends.setUsername(Constant.NEW_FRIENDS_USERNAME);
-        String strChat = getResources().getString(
-                R.string.Application_and_notify);
-        newFriends.setNick(strChat);
-
-        userlist.put(Constant.NEW_FRIENDS_USERNAME, newFriends);
-        // 添加"群聊"
-        User groupUser = new User();
-        String strGroup = getResources().getString(R.string.group_chat);
-        groupUser.setUsername(Constant.GROUP_USERNAME);
-        groupUser.setNick(strGroup);
-        groupUser.setHeader("");
-        userlist.put(Constant.GROUP_USERNAME, groupUser);
-
-        // 添加"Robot"
-        User robotUser = new User();
-        String strRobot = getResources().getString(R.string.robot_chat);
-        robotUser.setUsername(Constant.CHAT_ROBOT);
-        robotUser.setNick(strRobot);
-        robotUser.setHeader("");
-        userlist.put(Constant.CHAT_ROBOT, robotUser);
-
-        // 存入内存
-        //PsApplication.getInstance().setContactList(userlist);
-        // 存入db
-        UserDao dao = new UserDao(this);
-        List<User> users = new ArrayList<User>(userlist.values());
-        dao.saveContactList(users);
     }
 
     /**
@@ -585,14 +312,10 @@ public class HomeActivity extends BaseActivity implements ViewPagerEx.OnPageChan
             Log.i("refreshUI", unreadmsgcount + "");
         }
         if (unreadmsgcount > 0) {
-  //          tv_unread_msg_number.setText(unreadmsgcount + "");
-//            tv_unread_msg_number.setVisibility(View.VISIBLE);
             homeItemPresenter.changeMarkCount(unreadmsgcount);
         } else {
             homeItemPresenter.changeMarkCount(0);
-//            tv_unread_msg_number.setVisibility(View.GONE);
         }
-        //tv_unread_msg_number.invalidate();
     }
 
 }
